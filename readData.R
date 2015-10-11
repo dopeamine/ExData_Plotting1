1d294498fff9be57f31191744928e6d94d94ee98
Shantanu <shantanu.r@designnxt.com>
2015-10-12 00:09:49 +0530

    repaired silly mistake

--- readData.R
+++ readData.R
@@ -1,8 +1,8 @@
-pollutionData<-read.table("household_power_consumption.txt",sep = ";",header = TRUE,na.strings = '?')
-pollutionData$Date<-as.Date(pollutionData$Date,"%d/%m/%Y")
+powerConsumptionData<-read.table("household_power_consumption.txt",sep = ";",header = TRUE,na.strings = '?')
+powerConsumptionData$Date<-as.Date(powerConsumptionData$Date,"%d/%m/%Y")
 date1<-as.Date("2007/02/01","%Y/%m/%d")
 date2<-as.Date("2007/02/02","%Y/%m/%d")
-mainData<-pollutionData[pollutionData$Date>=date1 & pollutionData$Date<=date2,]
+mainData<-powerConsumptionData[powerConsumptionData$Date>=date1 & powerConsumptionData$Date<=date2,]
 # combine date time to create POSIX style date-time array
 dateTime<-paste(mainData$Date,mainData$Time)
 dateTime<-strptime(dateTime,"%Y-%m-%d %H:%M:%S")
